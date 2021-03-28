702c3efbe6aff584a0406ddefed34ec70f6d89f6
kentaro-seki <mad8ai7h02031987seki@gmail.com>
2021-03-28 07:51:09 +0000

    Laravel12

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'MYプロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MYプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
