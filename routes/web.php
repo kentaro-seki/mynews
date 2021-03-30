db5dab90545d146407cd37916e8c8b13082963df
kentaro-seki <mad8ai7h02031987seki@gmail.com>
2021-03-30 11:53:06 +0000

    laravel14-15課題用

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,8 @@ Route::post('profile/edit', 'Admin\profileController@update');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+});
